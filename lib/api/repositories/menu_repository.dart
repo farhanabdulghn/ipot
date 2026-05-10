e17c7e6b1cf36a1a69ab8d078c710377ca1256ad
Farhan Ab.G <farhannamikaze18@gmail.com>
2026-05-10 20:57:05 +0700

    feat: add menu repository

---
+++ lib/api/repositories/menu_repository.dart
@@ -0,0 +1,18 @@
+import 'package:ipot/api/api_sever.dart';
+import 'package:ipot/models/menu/menu_model.dart';
+
+class MenuRepository {
+  final _apiServer = ApiServer.getInstance();
+
+  Future<MenuModel?> getMenuByTableId(String? tableId) async {
+    final queryParams = <String, dynamic>{'table_id': tableId};
+
+    final response = await _apiServer.callService(
+      requestType: RequestType.get,
+      endPoint: '/menu',
+      queryParams: queryParams,
+    );
+
+    return MenuModel.fromJson(response.data);
+  }
+}
