d6c093fb341720ae15b199c45b6844f89ebf472a
Farhan Ab.G <farhannamikaze18@gmail.com>
2026-05-15 19:14:31 +0700

    feat: add generated app routes and route generator tool

---
+++ lib/app_routes.dart
@@ -0,0 +1,18 @@
+// GENERATED FILE - DO NOT MODIFY BY HAND
+// Run with: dart lib/tools/generate_routes.dart
+
+import 'package:flutter/material.dart';
+import 'package:ipot/screens/scanner_screen.dart';
+
+// ignore_for_file: constant_identifier_names
+
+class AppRoute {
+  static String scanner = '/scanner';
+}
+
+class AppRoutes {
+  static final Map<String, WidgetBuilder> routes = {
+    AppRoute.scanner: (context) =>  ScannerScreen(),
+  };
+}
+
